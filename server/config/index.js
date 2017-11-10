caf12cc1ee2298c01b49149c11e949562f24c9bc
junhee.ko <junhee.ko@ikoob.com>
2017-11-10 12:52:02 +0900

    config modified

--- server/config/index.js
+++ server/config/index.js
@@ -1,32 +1,32 @@
 var config = {
     name: 'API',
     server: {
-        host: '0.0.0.0',
-        port: 8000,
+        host: 'host',
+        port: port,
         labels: ['api'],
         routes: {
             cors: { credentials: 'true' }
         }
     },
     openApi: {
-        baseUrl: 'http://openapi.jeonju.go.kr/rest/',
-        
-       storeServiceId: 'jeonjufood/',
-        stayServiceId: 'hanokhouse/',
-        
-       storeOpList: 'getHanOkFoodList?',
-        stayOpList: 'getHanokHouseList?',
-        stayOpImg: 'getHanokHosueFile?',
-        authApiKey: 'DFQWXYPYQVYFPGN'
+        baseUrl: 'baseUrl',
+
+        storeServiceId: 'storeServiceId',
+        stayServiceId: 'stayServiceId',
+
+        storeOpList: 'storeOpList',
+        stayOpList: 'stayOpList',
+        stayOpImg: 'stayOpImg',
+        authApiKey: 'authApiKey'
     },
     database: {
         mongo:
         {
-            host: 'ds249545.mlab.com',
-            username: 'junhee.ko',
-            password: 'qq1212qq1212!',
-            port: '49545',
-            database: 'jj_db'
+            host: 'host',
+            username: 'username',
+            password: 'password',
+            port: 'port',
+            database: 'database'
         }
     }
 };
